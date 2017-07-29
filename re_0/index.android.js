083817240969868967b9910df3161ed5f9d7443b
kriys94350 <christian.trn@gmail.com>
2017-07-29 17:51:28 +0200

    add files for android

--- re_0/index.android.js
+++ re_0/index.android.js
@@ -17,7 +17,7 @@ export default class re_0 extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Welcome to React Native! Test u
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.android.js
