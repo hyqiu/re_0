5cd84ba0a1c0648ba80d08fbebbb946899e445fb
Henri Qiu <qiu.henri@gmail.com>
2017-07-30 15:15:17 +0200

    added boilerplate

---
+++ re_0/src/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import {Header} from './components/common'
+
+const App = () => {
+  return (
+    <Text>Hello World</Text>
+  );
+};
+
+export default App;
